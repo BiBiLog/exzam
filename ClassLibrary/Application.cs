142f595ae7708feea193150f629a4f8a8b501958
Vadimka133719 <molochko154@gmail.com>
2022-04-19 09:26:26 +0300

    exzamen

---
+++ ClassLibrary/Application.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Application
+    {
+        private Application application;
+        private List<TaskList> taskList;
+
+        
+
+        public Application GetApplication()
+        {
+            if (application != null)
+            {
+                return application;
+            }
+            application = new Application();
+            return application;
+        }
+
+        
+        public TaskList GetTaskListName (string name)
+        {
+            return taskList.FirstOrDefault(t => t.GetName() == name);
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ClassLibrary;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TaskList taskList = new TaskList("Новый список задач");
+            Assert.AreEqual("Новый список задач", taskList.GetName());
+        }
+    }
+}
